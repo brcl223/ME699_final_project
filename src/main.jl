9f54d304703626fda34eb84ea5924e779b9af002
Benton Clark <brcl223@g.uky.edu>
2020-04-23 21:24:20 -0400

    Switch from Panda to 3DOF robot

--- src/main.jl
+++ src/main.jl
@@ -1,31 +1,25 @@
-import Pkg;
-# Pkg.activate(@__DIR__);
-using RigidBodyDynamics, RigidBodySim, DifferentialEquations
-using LinearAlgebra
-using StaticArrays
-#using Plots
-using MeshCat, MeshCatMechanisms
+using RigidBodyDynamics
+using MeshCatMechanisms
 
-include("./PandaRobot.jl")
-
-panda = PandaRobot.Panda()
-mechanism = panda.mechanism
+urdf = joinpath(".", "robot.urdf")
+mechanism = parse_urdf(urdf)
 state = MechanismState(mechanism)
-set_configuration!(state, [0.0; 0.0; 0.0; 0.0; 0.0; pi/2.0; 0.0])
-zero_velocity!(state)
 
-function control!(τ, t, state)
-    # Do some PD
-    τ .= -20 .* velocity(state) - 100*(configuration(state) - [0.0; 0.0; 0.0; pi/2.0; 0.0; pi/2.0; 0.0; 0.0; 0.0])
+@show mechanism
+@show configuration(state)
+
+function control!(τ::AbstractVector, t, state::MechanismState)
+    desired_state = [pi/2.; pi/2.; pi/2.]
+    τ .= -20 .* (configuration(state) - desired_state) - 10 .* velocity(state)
 end
 
-final_time = 10.
-ts, qs, vs = simulate(state, final_time, control!; Δt=1e-3)
-mvis = MechanismVisualizer(mechanism, URDFVisuals(PandaRobot.urdfpath()))
+mvis = MechanismVisualizer(mechanism, URDFVisuals(urdf))
 open(mvis)
-animate(mvis, ts, qs; realtimerate=1.)
+
+final_time = 10.
+ts, qs, vs = simulate(state, final_time, control!; Δt = 1e-3)
 
 while true
-    sleep(15)
-    animate(mvis, ts, qs; realtimerate=1.)
+    animate(mvis, ts, qs; realtimerate = 1.)
+    sleep(5)
 end
